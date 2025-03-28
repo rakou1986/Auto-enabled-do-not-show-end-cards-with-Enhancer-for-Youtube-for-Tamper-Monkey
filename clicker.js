d8d1fdc63629ee9ea3eec5dd504f5fa4daeb2700
rakou1986 <rakou1986@gmail.com>
2025-03-28 14:22:58 +0900

    added

---
+++ clicker.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         set enabled efyt-cards-end-screens automatically
+// @namespace    http://tampermonkey.net/
+// @version      2025-03-28
+// @description  Automatically set "do not show end screen cards" when using "Enhancer for Youtube"
+// @author       rakou
+// @match        https://www.youtube.com/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let intervalId = null;
+    function checkAndClick() {
+        const targetElement = document.querySelector('#efyt-cards-end-screens');
+        if (targetElement) {
+            targetElement.click();
+            clearInterval(intervalId);
+        }
+    }
+
+    intervalId = setInterval(checkAndClick, 5000);
+})();
